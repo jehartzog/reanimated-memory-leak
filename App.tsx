e39127cd85f1022a3617fac84fa697e6e051c884
EricH <AvarioDeveloper@gmail.com>
2020-05-01 13:41:44 -0600

    Add toggle

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import LeakyComponent from './LeakyComponent';
 
 export default function App() {
+
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <LeakyComponent />
+      <TouchableOpacity onPress={() => setIsVisible(!isVisible)}>
+        <Text>Press me to toggle visible component, and leak</Text>
+      </TouchableOpacity>
+      {isVisible && <LeakyComponent />}
     </View>
   );
 }
